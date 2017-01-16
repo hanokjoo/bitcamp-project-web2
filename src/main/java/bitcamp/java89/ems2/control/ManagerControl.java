17be8bb1fc5232f632007085a349b6bc985163ca
hanokjoo <okjoo.han88@gmail.com>
2017-01-16 16:47:39 +0900

    .

--- src/main/java/bitcamp/java89/ems2/control/ManagerControl.java
+++ src/main/java/bitcamp/java89/ems2/control/ManagerControl.java
@@ -1,7 +1,7 @@
 package bitcamp.java89.ems2.control;
 
 import java.io.File;
-import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
@@ -12,25 +12,19 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
 
-import bitcamp.java89.ems2.dao.ManagerDao;
-import bitcamp.java89.ems2.dao.MemberDao;
-import bitcamp.java89.ems2.dao.StudentDao;
-import bitcamp.java89.ems2.dao.TeacherDao;
 import bitcamp.java89.ems2.domain.Manager;
-import bitcamp.java89.ems2.domain.Member;
+import bitcamp.java89.ems2.service.ManagerService;
 import bitcamp.java89.ems2.util.MultipartUtil;
 
 @Controller
 public class ManagerControl {
   @Autowired ServletContext sc;
-  @Autowired ManagerDao managerDao;
-  @Autowired MemberDao memberDao;
-  @Autowired TeacherDao teacherDao;
-  @Autowired StudentDao studentDao;
+  
+  @Autowired ManagerService managerService;
   
   @RequestMapping("/manager/list")
   public String list(Model model) throws Exception {
-    ArrayList<Manager> list = managerDao.getList();
+    List<Manager> list = managerService.getList();
     model.addAttribute("managers", list);
     model.addAttribute("title", "매니저 관리-목록");
     model.addAttribute("contentPage", "/manager/list.jsp");
@@ -40,7 +34,7 @@ public class ManagerControl {
   
   @RequestMapping("/manager/detail")
   public String detail(int memberNo, Model model) throws Exception {
-    Manager manager = managerDao.getOne(memberNo);
+    Manager manager = managerService.getDetail(memberNo);
     if (manager == null) {
       throw new Exception("사용자가 없습니다.");
     }
@@ -53,17 +47,7 @@ public class ManagerControl {
   
   @RequestMapping("/manager/add")
   public String add(Manager manager, MultipartFile photo) throws Exception {
-    if (managerDao.count(manager.getEmail()) < 0) {
-      throw new Exception("같은 이메일이 존재합니다. 등록을 취소합니다.");
-    }
-    
-    if (memberDao.count(manager.getEmail()) == 0) { // 강사나 학생으로 등록되지 않았다면,
-      memberDao.insert(manager);
-    } else { // 강사나 학생으로 이미 등록된 사용자라면 기존의 회원 번호를 사용한다.
-      Member member = memberDao.getOne(manager.getEmail());
-      manager.setMemberNo(member.getMemberNo());
-    }
-    if (photo.getSize() > 0) { // 파일이 업로드 되었다면
+    if (photo.getSize() > 0) {
       String newFilename = MultipartUtil.generateFilename();
       photo.transferTo(new File(sc.getRealPath("/upload/" + newFilename)));
       manager.setPhotoPath(newFilename);
@@ -71,34 +55,20 @@ public class ManagerControl {
       photo.transferTo(new File(sc.getRealPath("/upload/default.png")));
       manager.setPhotoPath("default.png");
     }
-    managerDao.insert(manager);
+    managerService.add(manager);
 
     return "redirect:list.do";
   }
   
   @RequestMapping("/manager/delete")
   public String delete(int memberNo, HttpServletRequest request) throws Exception {
-    if (managerDao.countByNo(memberNo) == 0) {
-      throw new Exception("사용자를 찾지 못했습니다.");
-    }
-    
-    managerDao.delete(memberNo);
-    
-    if (studentDao.countByNo(memberNo) == 0 && teacherDao.countByNo(memberNo) == 0) {
-      memberDao.delete(memberNo);
-    }
-
+    managerService.delete(memberNo);
     return "redirect:list.do";
   }
   
   @RequestMapping("/manager/update")
   public String update(Manager manager, MultipartFile photo) throws Exception {
-    if (managerDao.countByNo(manager.getMemberNo()) == 0) {
-      throw new Exception("사용자를 찾지 못했습니다.");
-    }
-    memberDao.update(manager);
-    
-    if (photo.getSize() > 0) { // 파일이 업로드 되었다면
+    if (photo.getSize() > 0) {
       String newFilename = MultipartUtil.generateFilename();
       photo.transferTo(new File(sc.getRealPath("/upload/" + newFilename)));
       manager.setPhotoPath(newFilename);
@@ -106,7 +76,7 @@ public class ManagerControl {
       photo.transferTo(new File(sc.getRealPath("/upload/default.png")));
       manager.setPhotoPath("default.png");
     }
-    managerDao.update(manager);
+    managerService.update(manager);
 
     return "redirect:list.do";
   }
--- src/main/java/bitcamp/java89/ems2/control/TeacherControl.java
+++ src/main/java/bitcamp/java89/ems2/control/TeacherControl.java
@@ -13,27 +13,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
 
-import bitcamp.java89.ems2.dao.ManagerDao;
-import bitcamp.java89.ems2.dao.MemberDao;
-import bitcamp.java89.ems2.dao.StudentDao;
-import bitcamp.java89.ems2.dao.TeacherDao;
-import bitcamp.java89.ems2.domain.Member;
 import bitcamp.java89.ems2.domain.Photo;
 import bitcamp.java89.ems2.domain.Teacher;
+import bitcamp.java89.ems2.service.TeacherService;
 import bitcamp.java89.ems2.util.MultipartUtil;
 
 @Controller
 public class TeacherControl {
   @Autowired ServletContext sc;
-  @Autowired TeacherDao teacherDao;
-  @Autowired MemberDao memberDao;
-  @Autowired ManagerDao managerDao;
-  @Autowired StudentDao studentDao;
   
+  @Autowired TeacherService teacherService;
 
   @RequestMapping("/teacher/list")
   public String list(Model model) throws Exception {
-    ArrayList<Teacher> list = teacherDao.getList(); 
+    List<Teacher> list = teacherService.getList(); 
     model.addAttribute("teachers", list);
     model.addAttribute("title", "강사 관리-목록");
     model.addAttribute("contentPage", "/teacher/list.jsp");
@@ -43,7 +36,7 @@ public class TeacherControl {
   
   @RequestMapping("/teacher/detail")
   public String detail(int memberNo, Model model) throws Exception {
-    Teacher teacher = teacherDao.getOneWithPhoto(memberNo);
+    Teacher teacher = teacherService.getDetail(memberNo);
     if (teacher == null) {
       throw new Exception("사용자가 없습니다.");
     }
@@ -65,17 +58,6 @@ public class TeacherControl {
     multi.add(photo2);
     multi.add(photo3);
     
-    if (teacherDao.count(teacher.getEmail()) > 0) {
-      throw new Exception("같은 이메일이 존재합니다. 등록을 취소합니다.");
-    }
-    
-    if (memberDao.count(teacher.getEmail()) == 0) { // 학생이나 매니저로 등록되지 않았다면,
-      memberDao.insert(teacher);
-    } else { // 학생이나 매니저로 이미 등록된 사용자라면 기존의 회원 번호를 사용한다.
-      Member member = memberDao.getOne(teacher.getEmail());
-      teacher.setMemberNo(member.getMemberNo());
-    }
-    
     ArrayList<Photo> photoList = new ArrayList<>();
     for (MultipartFile mf : multi) {
       if (mf.getSize() > 0) { // 파일이 업로드 되었다면
@@ -86,25 +68,14 @@ public class TeacherControl {
     }
     teacher.setPhotoList(photoList);
     
-    teacherDao.insert(teacher);
-    teacherDao.insertPhotoList(teacher);
+    teacherService.add(teacher);
 
     return "redirect:list.do";
   }
   
   @RequestMapping("/teacher/delete")
   public String delete(int memberNo, HttpServletRequest request) throws Exception {
-    if (teacherDao.countByNo(memberNo) == 0) {
-      throw new Exception("사용자를 찾지 못했습니다.");
-    }
-    
-    teacherDao.deletePhotoList(memberNo);
-    teacherDao.delete(memberNo);
-    
-    if (managerDao.countByNo(memberNo) == 0 && studentDao.countByNo(memberNo) == 0) {
-      memberDao.delete(memberNo);
-    }
-
+    teacherService.delete(memberNo);
     return "redirect:list.do";
   }
   
@@ -115,11 +86,6 @@ public class TeacherControl {
     multi.add(photo2);
     multi.add(photo3);
     
-    if (teacherDao.countByNo(teacher.getMemberNo()) == 0) {
-      throw new Exception("사용자를 찾지 못했습니다.");
-    }
-    memberDao.update(teacher);
-    
     ArrayList<Photo> photoList = new ArrayList<>();
     for (MultipartFile mf : multi) {
       if (mf.getSize() > 0) { // 파일이 업로드 되었다면
@@ -129,8 +95,7 @@ public class TeacherControl {
       }
     }
     teacher.setPhotoList(photoList);
-    teacherDao.update(teacher);
-    teacherDao.insertPhotoList(teacher);
+    teacherService.update(teacher);
 
     return "redirect:list.do";
   }
